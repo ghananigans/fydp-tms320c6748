255e9edc0d14c2caf168cc7ffc1c58d83f52630d
Rishi Kapadia <r4kapadi@uwaterloo.ca>
2017-03-14 20:40:41 -0400

    Antivoice on board matches console-sw.  Still need to make it it's own app on uart.

--- src/main.c
+++ src/main.c
@@ -20,6 +20,97 @@
 #include <math.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include "antivoice/common.h"
+#include "antivoice/mode_of_operation.h"
+#include "antivoice/mic_input.h"
+#include "antivoice/speaker_output.h"
+#include "antivoice/fft_wrap.h"
+#include "antivoice/signal_processing.h"
+#include "antivoice/matlab_helper.h"
+
+#define MATLAB_DEBUG_ENABLED 1
+
+unsigned int iteration_count = 0;
+#define NUM_SIMULATION_CYCLES 400
+
+
+void run_calibration()
+{
+    unsigned int i;
+
+    /*Populate calibration data structures.
+        TODO: perhaps replace this function with a separate header file altogether with an assignment of calibration results to the calibration variables.
+            create calibration.h
+    */
+    for(i = 0; i < FFT_SIZE; i++)
+    {
+        phase_sys[i] = 0.0001f;
+        mag_sys[i] = 1.0f;
+    }
+
+    NORMAL_PRINT("run_calibration() complete.\n");
+}
+
+void run_main_algorithm()
+{
+    float mic_data_float[AUDIO_CHANNEL_COUNT]; /* gets populated with newest mic samples as float */
+    unsigned int ch;
+
+    /*Get mic input */
+    /*  returns an array of floats, 1 float per mic
+        this is assuming that reading McBSP buffered input takes minimal clock cycles, otherwise we may need to process mic one by one
+    */
+    get_mic_data_float(mic_data_float);
+
+    /*NORMAL_PRINT("run_main_algorithm audio_data[01].x[0] = %f, %f \n", audio_data[0].x[0], audio_data[1].x[0]);*/
+    /* append to working signal to operate on (signal is allocated in common.h) */
+    add_new_mic_data(mic_data_float);
+    /*NORMAL_PRINT("run_main_algorithm mic_data_float = %f, %f \n", mic_data_float[0], mic_data_float[1]);*/
+
+
+    for(ch = 0; ch < AUDIO_CHANNEL_COUNT; ch++)
+    {
+        /* NOTE: hilbert transform is performed by fft_wrap to make analytical signal X*/
+        fft_wrap(audio_data[ch].x,audio_data[ch].X);
+
+        /* copy X to Y, we leave X untouched for debugging purposes */
+        memcpy(audio_data[ch].Y, audio_data[ch].X, M*sizeof(float));
+
+        /* Apply corresponding phase adjustments to Y */
+        /*TODO: change to apply_mag_phase_adj() once we figure out how to apply mag adjustments properly
+            for a complex number in the freq domain */
+        apply_phase_adj(audio_data[ch].Y);
+
+        /*Take ifft of Y to get output y in time domain */
+        ifft_wrap(audio_data[ch].Y,audio_data[ch].y);
+
+        /*Write to speakers*/
+        send_speaker_output(audio_data[ch].y);
+    }
+
+#if MATLAB_DEBUG_ENABLED
+    if(iteration_count == 300)
+    {
+        NORMAL_PRINT("x signal:\n");
+        matlab_float_print_complex(audio_data[0].x, M);
+        NORMAL_PRINT("\n\n\n\n X signal: \n");
+        matlab_float_print_complex(audio_data[0].X, M);
+
+        NORMAL_PRINT("\n\n\n\n phase_adj signal: \n");
+        matlab_float_print_array(phase_adj, FFT_SIZE);
+
+        NORMAL_PRINT("\n\n\n\n Y signal: \n");
+        matlab_float_print_complex(audio_data[0].Y, M);
+        NORMAL_PRINT("\n\n\n\n y signal: \n");
+        matlab_float_print_complex(audio_data[0].y, M);
+    }
+#endif /* MATLAB_DEBUG_ENABLED */
+
+
+}
+
+
+
 
 #define CHANNEL_POWER DAC_POWER_ON_CHANNEL_7
 #define CHANNEL DAC_ADDRESS_CHANNEL_7
@@ -249,9 +340,7 @@ play_mic_to_dac (
 #define NUM_COMMANDS (4)
 
 int
-main (
-    void
-    )
+main (    void     )
 {
     int ret_val;
     console_command_t const commands[NUM_COMMANDS] = {
@@ -354,6 +443,38 @@ main (
      */
     ret_val = console_commands_run();
     ASSERT(ret_val == CONSOLE_COMMANDS_OK, "Console commands run failed! (%d)\n", ret_val);
+
+
+    /*Calibration switch must be enabled at power-on to calibrate
+        switch does not have effect during normal mode of operation (likely accidental if switched)*/
+        if(is_calibration_enabled())
+        {
+            run_calibration();
+            /*TODO: perhaps have a pause here? */
+        }
+
+        fft_wrap_init();
+        signal_processing_init();
+
+        while(iteration_count < NUM_SIMULATION_CYCLES)
+        {
+            /*wait for algorithm to be enabled (controlled by board switch)
+            user controls whether regular headphones listening mode, or antivoice mode */
+            if(is_algorithm_enabled())
+            {
+                run_main_algorithm();
+            }
+            else
+            {
+                /*sleep to save power if possible / easy? */
+            }
+            /*NOTE: we may split up is_algorithm_enabled into voiceObfuscationEnabled and ancEnabled as they are 2 different modes of operation*/
+
+            iteration_count++;
+        }
+        NORMAL_PRINT("Completed %u simulation iterations (samples).\n", NUM_SIMULATION_CYCLES);
+
+
 
     /*
      * Loop forever.
